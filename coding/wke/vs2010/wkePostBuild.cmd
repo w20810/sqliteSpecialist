98555a867e4eecaeff0200291f5d1d9bad23099c
w20810 <376365042@qq.com>
2016-08-16 11:32:01 +0800

    remove wke

--- coding/wke/vs2010/wkePostBuild.cmd
+++
@@ -1,6 +0,0 @@
-mkdir 2>NUL "..\demo\%CONFIGURATIONNAME%\bin"
-mkdir 2>NUL "..\demo\%CONFIGURATIONNAME%\libwke"
-
-xcopy /D /F /R /Y "%TARGETPATH%" "..\demo\%CONFIGURATIONNAME%\bin\"
-xcopy /D /F /R /Y "%CONFIGURATIONBUILDDIR%\lib\wke.lib" "..\demo\%CONFIGURATIONNAME%\libwke\"
-xcopy /D /F /R /Y "..\wke\wke.h" "..\..\..\shell\uilib\control\wke\"
